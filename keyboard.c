aa5debb4b3411266adc167fb534901269854e5a0
Simrat Grewal <simrats169169@gmail.com>
2022-08-31 18:24:59 -0700

    keyboard: Add alt+1 and alt+2 bindings for workspace debugging

--- keyboard.c
+++ keyboard.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #include <unistd.h>
+#include <wayland-server-protocol.h>
 #include <wlr/util/log.h>
 #include <xkbcommon/xkbcommon-keysyms.h>
 #include <xkbcommon/xkbcommon.h>
@@ -19,11 +20,23 @@ void on_modifiers(struct wl_listener *listener, void *data) {
                                      &keyboard->wlr_keyboard->modifiers);
 }
 
+bool handle_alt_press(xkb_keysym_t sym) {
+  switch (sym) {
+  case XKB_KEY_1:
+    wlr_log(WLR_INFO, "Logo + 1");
+    break;
+  case XKB_KEY_2:
+    wlr_log(WLR_INFO, "Logo + 2");
+    break;
+  default:
+    return false;
+  }
+  return true;
+}
+
 void on_key(struct wl_listener *listener, void *data) {
   struct simwm_keyboard *keyboard = wl_container_of(listener, keyboard, key);
   struct wlr_keyboard_key_event *event = data;
-
-  wlr_log(WLR_INFO, "Key Pressed: %d", event->keycode);
 
   // libinput -> xkb
   uint32_t keycode = event->keycode + 8;
@@ -31,10 +44,22 @@ void on_key(struct wl_listener *listener, void *data) {
   int nsyms =
       xkb_state_key_get_syms(keyboard->wlr_keyboard->xkb_state, keycode, &syms);
   uint32_t modifiers = wlr_keyboard_get_modifiers(keyboard->wlr_keyboard);
+  bool handled = false;
 
-  wlr_seat_set_keyboard(server->seat, keyboard->wlr_keyboard);
-  wlr_seat_keyboard_notify_key(server->seat, event->time_msec, event->keycode,
-                               event->state);
+  if ((modifiers & WLR_MODIFIER_ALT) &&
+      event->state == WL_KEYBOARD_KEY_STATE_PRESSED) {
+    /* If alt is held down and this button was _pressed_, we attempt to
+     * process it as a compositor keybinding. */
+    for (int i = 0; i < nsyms; i++) {
+      handled = handle_alt_press(syms[i]);
+    }
+  }
+
+  if (!handled) {
+    wlr_seat_set_keyboard(server->seat, keyboard->wlr_keyboard);
+    wlr_seat_keyboard_notify_key(server->seat, event->time_msec, event->keycode,
+                                 event->state);
+  }
 }
 
 void server_new_keyboard(struct wlr_input_device *device) {
