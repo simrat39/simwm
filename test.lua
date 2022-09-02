10842c0e66e122ae066eb0d546b038a62a0ad5de
Simrat Grewal <simrats169169@gmail.com>
2022-09-02 14:39:17 -0700

    default config updates

--- test.lua
+++ test.lua
@@ -1,18 +1,16 @@
-local inspect = require("inspect")
-
-print("hiu");
--- print(inspect(get_outputs()))
-
+local inspect  = require("inspect")
 
 local outputs = get_outputs()
 
-spawn("swaybg -o \\* -i ~/Media/macos-monterey-2560x1440_897687-mm-90.jpg -m fill")
+print(inspect(outputs))
+
+-- spawn("swaybg -o \\* -i ~/Media/macos-monterey-2560x1440_897687-mm-90.jpg -m fill")
 
 for i = 1, 10, 1 do
-  add_workspace(tostring(i))
+  add_workspace(i)
 
-  add_keymap({"Alt"}, tostring(i), function ()
-    outputs[1]:set_workspace(tostring(i))
+  add_keymap({"Alt"}, i, function ()
+    outputs[1]:set_workspace(i)
   end)
 end
 
