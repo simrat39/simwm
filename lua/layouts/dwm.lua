403886ff9e1058fadfb15859a884b84e4acaba69
Simrat Grewal <simrats169169@gmail.com>
2022-09-03 18:36:00 -0700

    layouts: Remove arrange code
    Will come back later probs idk

--- lua/layouts/dwm.lua
+++ lua/layouts/dwm.lua
@@ -61,28 +61,6 @@ end
 ---Logic to arrange views in a workspae.
 ---@param output Output Output on which the workspace resides
 ---@param workspace Workspace Workspace to arrange the views in
-function M.arrange(output, workspace)
-  local views = workspace:get_views()
-  local res = output:get_resolution()
-
-  if #views == 1 then
-  elseif #views > 1 then
-    local master_width = res.width * 0.45
-    local child_count = #views - 1
-
-    local child_width = res.width - master_width
-    local child_height = res.height / (child_count)
-
-    for index, view in ipairs(views) do
-      if index == 1 then
-        view:set_pos(0, 0)
-        view:set_size(master_width, res.height)
-      else
-        view:set_pos(master_width, child_height * (index - 2))
-        view:set_size(child_width, child_height)
-      end
-    end
-  end
-end
+function M.arrange(output, workspace) end
 
 return M
