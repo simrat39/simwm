3e996ed681ec1006f4f655dc910d5ec3eaced6d3
Simrat Grewal <simrats169169@gmail.com>
2022-09-04 16:04:32 -0700

    layout: Support multiple workspaces

--- lua/layouts/dwm.lua
+++ lua/layouts/dwm.lua
@@ -1,102 +1,138 @@
+local inspect = require("inspect")
+
 ---@class DwmLayout : Layout
 local M = {
   name = "Dwm",
-  master = nil,
+  master = {},
   stack = {},
   width = 0.5,
 }
 
+---Get the stack for a workspace
+---@param ws any
+---@return any[]
+local function get_stack(ws)
+ return M.stack[ws.name]
+end
+
+---Sets the stack for a workspace.
+---@param ws Workspace
+---@param new any[]
+local function set_stack(ws, new)
+ M.stack[ws.name] = new;
+end
+
+---Gets the master for a workspace.
+---@param ws Workspace
+local function get_master(ws)
+ return M.master[ws.name];
+end
+
+---Sets the master for a workspace.
+---@param ws Workspace
+---@param new any
+local function set_master(ws, new)
+ M.master[ws.name] = new;
+end
+
 ---Used to maximize the window when we only have one, which is the master.
----@param output Output
-local function set_master_fullscreen(output)
-  local res = output:get_resolution()
-  M.master:set_pos(5, 5)
-  M.master:set_size(res.width - 10, res.height - 10)
+---@param o Output
+---@param ws Workspace
+local function set_master_fullscreen(o, ws)
+  local res = o:get_resolution()
+  get_master(ws):set_pos(5, 5)
+  get_master(ws):set_size(res.width - 10, res.height - 10)
 end
 
 ---Called when we get atleast one view in the stack, so the master needs to be
 ---smaller now.
----@param output Output
-local function set_master_pos_width(output)
-  local res = output:get_resolution()
-  M.master:set_pos(0, 0)
-  M.master:set_size(res.width * M.width, res.height)
+---@param o Output
+---@param ws Workspace
+local function set_master_pos_width(o, ws)
+  local res = o:get_resolution()
+  get_master(ws):set_pos(0, 0)
+  get_master(ws):set_size(res.width * M.width, res.height)
 end
 
 ---Sets the stack position and width
----@param output Output
-local function set_stack(output)
-  local res = output:get_resolution()
+---@param o Output
+---@param ws Workspace
+local function set_stack_pos_size(o, ws)
+  local res = o:get_resolution()
   local master_width = res.width * M.width
-  local stack_count = #M.stack
+  local stack_count = #get_stack(ws)
 
   local stack_width = res.width - master_width
   local stack_height = res.height / (stack_count)
 
-  for index, view in ipairs(M.stack) do
+  for index, view in ipairs(get_stack(ws)) do
     view:set_pos(master_width, stack_height * (index - 1))
     view:set_size(stack_width, stack_height)
   end
 end
 
 ---Called when a new view is opened
----@param output Output
----@param workspace Workspace
+---@param o Output
+---@param ws Workspace
 ---@param view any
-function M.on_new_view(output, workspace, view)
-  if M.master == nil then
-    M.master = view;
-    set_master_fullscreen(output);
+function M.on_new_view(o, ws, view)
+  if not get_stack(ws) then
+    set_stack(ws, {})
+  end
+
+  if get_master(ws) == nil then
+    set_master(ws, view);
+    set_master_fullscreen(o, ws);
     return;
   end
 
-  if #M.stack then
-    set_master_pos_width(output);
-  end
+  table.insert(get_stack(ws), view)
+  set_stack_pos_size(o, ws);
 
-  table.insert(M.stack, view)
-  set_stack(output);
+  if #get_stack(ws) >= 1 then
+    set_master_pos_width(o, ws);
+  end
 end
 
 ---Called when a new view is opened
----@param output Output
----@param workspace Workspace
----@param view any
-function M.on_view_close(output, workspace, view)
+---@param o Output
+---@param ws Workspace
+---@param window any
+function M.on_view_close(o, ws, window)
   -- master was removed
-  if view.userdata == M.master.userdata then
+  if window.userdata == get_master(ws).userdata then
     -- check if we have any views in the stack
-    if #M.stack >= 1 then
+    if #get_stack(ws) >= 1 then
       -- Set a new master if we do
-      M.master = M.stack[1]
-      table.remove(M.stack, 1);
+      set_master(ws, get_stack(ws)[1])
+      table.remove(get_stack(ws), 1);
 
       -- after new master is set, do we have any views in the stack
-      if #M.stack >= 1 then
+      if #get_stack(ws) >= 1 then
         -- if we do, setup the layout normally
-        set_master_pos_width(output);
-        set_stack(output);
+        set_master_pos_width(o, ws);
+        set_stack_pos_size(o, ws);
       else
         -- if we don't, then fullscreen current master
-        set_master_fullscreen(output);
+        set_master_fullscreen(o, ws);
       end
     -- we don;t have any candidates for the new master, so do nothihng
     else
-      M.master = nil
+      set_master(ws, nil)
     end
   -- view from stack was removed
   else
-    for i, v in ipairs(M.stack) do
-      if v.userdata == view.userdata then
-        table.remove(M.stack, i);
+    for i, v in ipairs(get_stack(ws)) do
+      if v.userdata == window.userdata then
+        table.remove(get_stack(ws), i);
       end
     end
 
     -- stack was emptied
-    if #M.stack == 0 then
-      set_master_fullscreen(output);
+    if #get_stack(ws) == 0 then
+      set_master_fullscreen(o, ws);
     else
-      set_stack(output);
+      set_stack_pos_size(o, ws);
     end
   end
 end
